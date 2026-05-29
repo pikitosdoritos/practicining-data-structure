a6f79aef93cf47fe997e92421cb3349795cd056c
pikitosdoritos <fastovetsn@gmail.com>
2026-05-29 17:58:23 +0300

    first commit

---
+++ task1.py
@@ -0,0 +1,76 @@
+data = {
+    "users": [
+        {
+            "id": 1,
+            "name": "Nikita",
+            "age": 19,
+            "is_active": True,
+            "contacts": {
+                "email": "nikita@example.com",
+                "phone": "+380991112233"
+            },
+            "orders": [
+                {
+                    "id": 101,
+                    "status": "paid",
+                    "items": [
+                        {"title": "Keyboard", "price": 1200, "qty": 1},
+                        {"title": "Mouse", "price": 600, "qty": 2}
+                    ]
+                },
+                {
+                    "id": 102,
+                    "status": "pending",
+                    "items": [
+                        {"title": "Monitor", "price": 7000, "qty": 1}
+                    ]
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "name": "Anna",
+            "age": 22,
+            "is_active": False,
+            "contacts": {
+                "email": "anna@example.com"
+            },
+            "orders": [
+                {
+                    "id": 103,
+                    "status": "paid",
+                    "items": [
+                        {"title": "Laptop", "price": 30000, "qty": 1},
+                        {"title": "USB Cable", "price": 200, "qty": 3}
+                    ]
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "name": "Oleh",
+            "age": 17,
+            "is_active": True,
+            "contacts": {},
+            "orders": []
+        }
+    ],
+    "settings": {
+        "currency": "UAH",
+        "discounts": {
+            "student": 0.1,
+            "vip": 0.2
+        }
+    }
+}
+
+# function №1 (get name of users)
+def get_user_names(data):
+    list_of_names = []
+    for user in data["users"]:
+        list_of_names.append(user["name"])
+
+    return list_of_names
+
+
+print(get_user_names(data))
